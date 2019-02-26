87d4a142089f44f61e9f96eecd9c5c1274778c3f
filipposerio <filippo.serio@gmail.com>
2019-02-26 17:26:09 +0100

    login

--- src/js/login.js
+++ src/js/login.js
@@ -13,13 +13,12 @@ let objUser ={}
 const login_html = `
 <body>
     <div id="login">
-        <h3 class="text-center text-white pt-5">Login form</h3>
         <div class="container-fluid">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" name='search' class="form" action="" method="post">
-                            <h3 class="text-center text-info">Login - Gestionale D</h3>
+                            <h3 class="text-center text-info">Login - Figurine </h3>
                             <div class="form-group">
                                 <label for="username" class="text-info">Username:</label><br>
                                 <input type="text" name="username" id="username" class="form-control">
